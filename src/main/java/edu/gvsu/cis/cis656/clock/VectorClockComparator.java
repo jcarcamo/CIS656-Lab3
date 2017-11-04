79c7218454d512c62303939dccdea481ab322a6e
Juan Gonzalo Cárcamo <jcarcamo@gmail.com>
2017-11-04 18:37:53 -0400

    Implementation of comparator according to the problem definition

--- src/main/java/edu/gvsu/cis/cis656/clock/VectorClockComparator.java
+++ src/main/java/edu/gvsu/cis/cis656/clock/VectorClockComparator.java
@@ -7,13 +7,12 @@ public class VectorClockComparator implements Comparator<VectorClock> {
 
     @Override
     public int compare(VectorClock lhs, VectorClock rhs) {
-        int result = -1;
-    	for(String key:rhs.getClock().keySet()){
-        	int pid = Integer.parseInt(key);
-    		if(rhs.getTime(pid) < lhs.getTime(pid)){
-    			result = 1;
-    		}
-        }
+        int result = 0;
+    	if(lhs.happenedBefore(rhs)) {
+    		result = -1;
+    	}else if(rhs.happenedBefore(lhs)) {
+    		result = 1;
+    	}
     	return result;
     }
 }
