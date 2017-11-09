3ad6e7888f781e9df7058fcaf423feb76f7706a6
Juan Gonzalo Carcamo <jcarcamo@gmail.com>
2017-11-09 15:47:57 -0500

    packaging jar with shade plugin, fix message to match current snapshot name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ import edu.gvsu.cis.cis656.ui.ChatClient;
 public class App {
 	public static void main (String[] args){
 		if (args.length != 1) {
-            System.out.println("Usage: java ChatClient <username>");
+            System.out.println("Usage: java -jar lab3-0.0.1-SNAPSHOT.jar <username>");
             System.exit(-1);
         }
 		String username = args[0];
